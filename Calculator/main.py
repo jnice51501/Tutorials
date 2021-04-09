799607eec018b45195b6e315de16ac2803179451
Jnice51501 <jasondeamral19@gmail.com>
2021-04-09 09:01:52 -0400

    Basic info for python
    4hr lecture tutorials

---
+++ Calculator/main.py
@@ -0,0 +1,40 @@
+# uri = 20000
+# uriout = 25000
+
+# print("the cost to go to uri is " + str(uri))
+# tuitioncost = uri + uriout
+# print(tuitioncost)
+# if uri > uriout: 
+#     print("uri costs more than uriout")
+# elif uriout > uri: 
+#     print("uri out of state costs more than instate")
+# else:
+#     print("they cost the same")
+# jason = 21
+# if (21) >= 21:
+#     print("he is able to drink")
+# elif (21) > 18 and (21) < 21:
+#     print("he is able to join the military but not drink")
+# else:
+#     print("he is to young to do anything")
+
+# name = "jack, steve, jim, jas"
+# age = 15, 20, 10, 16
+
+
+# print("There was a boy his name was name")
+# print("He was only age years old.")
+
+# my_num = 20
+# print(str(my_num) + " is my favorite number")
+
+name = input("Enter your name here ")
+print("Hello, " +  name + (" what can we do for you today?"))
+response = input("")
+print ("you have entered " + response + (", correct?"))
+response2 = input("if Yes press Y if No press N" )
+print ("you have entered " + response2)
+if response2 == "Y":
+    print ("Gives us one moment while we connect you to an agent")
+elif response2 == "N":
+    print ("Then what seems to be the problem")
